3ce6e7ca08dbce4f2371b813dfeb2db04f703e87
NureAlam68 <nurealam151068@gmail.com>
2024-12-19 20:55:16 +0600

    delete operation done

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ async function run() {
       res.send(result);
     })
 
+    // get all jobs posted by a specific user
+    app.get('/jobs/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = { 'buyer.email': email }
+      const result = await jobsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // delete a job from db
+    app.delete('/job/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     
 
     // Send a ping to confirm a successful connection
