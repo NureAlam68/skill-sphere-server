602790263af299c1f1d66157b985f7c72e0f6b90
NureAlam68 <nurealam151068@gmail.com>
2024-12-19 22:24:21 +0600

    get data by id

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run() {
       res.send(result);
     })
 
+    // get a single job data by id from db
+    app.get('/job/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
     
 
     // Send a ping to confirm a successful connection
