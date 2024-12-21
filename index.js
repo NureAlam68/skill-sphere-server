005049c109ba87c24149794e506a98062c1548aa
NureAlam68 <nurealam151068@gmail.com>
2024-12-21 16:22:03 +0600

    update status

--- index.js
+++ index.js
@@ -102,6 +102,33 @@ async function run() {
       res.send(result)
     })
 
+    // get all bids for a specific user and get bid requests for a specific user
+    // using single api for two pages my bids and bid requests
+    app.get('/bids/:email', async(req, res) => {
+      const isBuyer = req.query.buyer;
+      const email = req.params.email;
+      let query = {};
+      if(isBuyer){
+        query.buyer = email;
+      } else{
+        query.email = email;
+      }
+      const result = await bidsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+     // update bid status
+     app.patch('/bid-status-update/:id', async (req, res) => {
+      const id = req.params.id
+      const { status } = req.body
+
+      const filter = { _id: new ObjectId(id) }
+      const updated = {
+        $set: { status },
+      }
+      const result = await bidsCollection.updateOne(filter, updated)
+      res.send(result)
+    })
 
     
 
